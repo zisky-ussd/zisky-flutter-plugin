e69374bd3d5eb45d57b00d284a534e04be8e53df
Marshall Chabanga <marshallchabanga@gmail.com>
2020-04-02 20:15:48 +0200

    send data back to application

---
+++ android/src/main/java/com/zisky/zisky/TransactionReceiver.java
@@ -0,0 +1,33 @@
+package com.zisky.zisky;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.HashMap;
+
+
+public class TransactionReceiver extends BroadcastReceiver {
+
+    public static final String TAG = "TransactionReceiver";
+
+    public TransactionReceiver() {
+
+    }
+
+    @Override
+    @SuppressWarnings("ConstantConditions")
+    public void onReceive(Context context, Intent intent) {
+
+
+        Log.e(TAG,"BROADCAST DATA RECEIVED");
+        HashMap<String, String> parsed_variables =
+                (HashMap<String, String>) intent.getSerializableExtra("parsed_variables");
+
+        Log.e(TAG, parsed_variables +"");
+
+
+    }
+
+}
--- android/src/main/java/com/zisky/zisky/ZiskyPlugin.java
+++ android/src/main/java/com/zisky/zisky/ZiskyPlugin.java
@@ -2,9 +2,11 @@ package com.zisky.zisky;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.net.Uri;
 import android.os.Build;
@@ -17,29 +19,52 @@ import com.zisky.sdk.model.USSDParameters;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import static com.zisky.sdk.Constants.USSD_MESSAGE;
+import static com.zisky.sdk.Constants.USSD_STATUS;
 import static com.zisky.zisky.PermissionsUtil.isAccessiblityServicesEnable;
 import static com.zisky.zisky.PermissionsUtil.openSettingsAccessibility;
 
 /**
  * ZiskyPlugin
  */
-public class ZiskyPlugin implements MethodCallHandler {
+public class ZiskyPlugin implements MethodCallHandler, EventChannel.StreamHandler, FlutterPlugin {
     /**
      * Plugin registration.
      */
     public static final String TAG = "ZiskyPlugin";
 
     private final Activity activity;
+    private Result resultLater;
+    private Context applicationContext;
+    private MethodChannel methodChannel;
+    private EventChannel eventChannel;
+    private BinaryMessenger messenger;
 
     public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "com.zisky.ussd.automation/zisky");
-        channel.setMethodCallHandler(new ZiskyPlugin(registrar.activity()));
+        ZiskyPlugin ziskyPlugin = new ZiskyPlugin(registrar.activity());
+        ziskyPlugin.onAttachedToEngine(registrar.context(), registrar.messenger());
+
+
+    }
+
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
+        Log.d(TAG, "onAttachedToEngine");
+        this.applicationContext = applicationContext;
+        methodChannel = new MethodChannel(messenger, "com.zisky.ussd.automation/zisky");
+        eventChannel = new EventChannel(messenger, "com.zisky.ussd.automation/zisky-stream");
+        eventChannel.setStreamHandler(this);
+        methodChannel.setMethodCallHandler(this);
+        this.messenger = messenger;
+
     }
 
     private ZiskyPlugin(Activity activity) {
@@ -48,21 +73,19 @@ public class ZiskyPlugin implements MethodCallHandler {
 
     @Override
     public void onMethodCall(MethodCall call, Result result) {
-        if (call.method.equals("getPlatformVersion")) {
-
-            result.success("Android " + android.os.Build.VERSION.RELEASE);
-
-
-        } else if (call.method.equals("callAction")) {
+        if (call.method.equals("callAction")) {
 
             proceedIfPermissionsAllowed(activity);
             final String actionId = call.argument("actionId");
             final HashMap<String, String> extras = call.argument("extras");
+            Log.d(TAG, "SUPPLIED ACTION_ID " + actionId);
+
             Intent intent = new USSDParameters
-                    .Builder(activity)
+                    .Builder(applicationContext)
                     .process(actionId)
                     .build();
             if (extras != null && !extras.isEmpty()) {
+                Log.d(TAG, "SUPPLIED EXTRAS " + extras.toString());
 
                 for (Map.Entry<String, String> entry : extras.entrySet()) {
                     String key = entry.getKey();
@@ -86,10 +109,8 @@ public class ZiskyPlugin implements MethodCallHandler {
 
                 }
             }
-            Log.d(TAG, "ACTION_ID " + actionId);
-            activity.startActivity(intent);
-            result.success("Android " + android.os.Build.VERSION.RELEASE);
-
+            applicationContext.startActivity(intent);
+            resultLater = result;
         }
         {
             result.notImplemented();
@@ -155,4 +176,71 @@ public class ZiskyPlugin implements MethodCallHandler {
             alertDialog.show();
         }
     }
+
+    private BroadcastReceiver messageReceiver;
+
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        onAttachedToEngine(binding.getApplicationContext(), messenger);
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding flutterPluginBinding) {
+        Log.d(TAG, "messageReceiver deregistered");
+
+        applicationContext = null;
+        eventChannel.setStreamHandler(null);
+        eventChannel = null;
+    }
+
+    public static final String INTENT_FILTER_PACKAGE = "zisky.com.test_automation.TRANSACTION_CONFIRMATION";
+
+    @Override
+    public void onListen(Object o, EventChannel.EventSink eventSink) {
+        messageReceiver = createBroadcastReceiver(eventSink);
+        IntentFilter intentFilter = new IntentFilter(INTENT_FILTER_PACKAGE);
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        applicationContext.registerReceiver(messageReceiver, intentFilter);
+
+    }
+
+    @Override
+    public void onCancel(Object o) {
+        Log.d(TAG, "messageReceiver cancelled");
+
+        if (messageReceiver != null) {
+            applicationContext.unregisterReceiver(messageReceiver);
+            messageReceiver = null;
+        }
+
+    }
+
+    private BroadcastReceiver createBroadcastReceiver(final EventChannel.EventSink events) {
+        return new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                HashMap<String, String> parsed_variables =
+                        (HashMap<String, String>) intent.getSerializableExtra("parsed_variables");
+
+                Log.d(TAG, "parsed_variables=" + parsed_variables != null ? parsed_variables.toString() : " " + " , status= " + intent.getStringExtra(USSD_STATUS));
+                if (intent.hasExtra(USSD_MESSAGE)
+                        && intent.hasExtra(USSD_STATUS)
+
+                ) {
+
+                    Map<String, Object> objectMap = new HashMap<>();
+                    objectMap.put("status", intent.getStringExtra(USSD_STATUS));
+                    objectMap.put("message", intent.getStringExtra(USSD_MESSAGE));
+                    objectMap.put("parsed_variables", parsed_variables);
+                    events.success(objectMap);
+                }
+
+
+            }
+        };
+
+    }
+
+
 }
